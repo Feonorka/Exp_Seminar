4cb65849a564e7460163157e796582ca00f0a6eb
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-26 00:57:21 +0300

    Написана программа LVL_1

--- Level_1/Program.cs
+++ Level_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int squareNum = number2 * number2;
+
+if (squareNum == number1)
+{
+    Console.Write($"Да. Число {number1} это квадрат числа {number2}");
+}
+else
+{
+    Console.Write($"Нет. Число {number1} не является квадратом числа {number2}");
+}
