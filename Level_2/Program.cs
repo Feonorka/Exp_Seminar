f2ca79deaf1e474ad3c131bd831e19be7046b2d6
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-26 01:17:47 +0300

    Написана программа LVL_2

--- Level_2/Program.cs
+++ Level_2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число от 1 до 7");
+Console.Write("Ваше число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7)
+{
+    Console.Write($"Введено некоректное число {number}");
+}
+if (number == 1)
+{
+    Console.Write($"Число {number} соответствует понедельнику");
+}
+if (number == 2)
+{
+    Console.Write($"Число {number} соответствует вторнику");
+}
+if (number == 3)
+{
+    Console.Write($"Число {number} соответствует среде");
+}
+if (number == 4)
+{
+    Console.Write($"Число {number} соответствует четвергу");
+}
+if (number == 5)
+{
+    Console.Write($"Число {number} соответствует пятнице");
+}
+if (number == 6)
+{
+    Console.Write($"Число {number} соответствует субботе");
+}
+if (number == 7)
+{
+    Console.Write($"Число {number} соответствует воскременью");
+}
