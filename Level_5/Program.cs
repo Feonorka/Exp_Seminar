556df77ccfae495a515aec2550a593d8d896f32b
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-26 02:27:31 +0300

    Написана программа LVL_7

--- Level_5/Program.cs
+++ Level_5/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите число");
-int MaxNum = Convert.ToInt32(Console.ReadLine());;
+int MaxNum = Convert.ToInt32(Console.ReadLine());
 
 int MinNum = MaxNum * -1;
 int Num = MinNum;
--- Level_7/Program.cs
+++ Level_7/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int res = Num % 10;
+Console.Write($"Последняя цифра числа {Num} это {res}");
