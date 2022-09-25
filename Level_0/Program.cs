5b7f57dd14463783f89dd8ffd9993183fac6506e
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-26 00:18:57 +0300

    Написан программа LVL_0

--- Level_0/Program.cs
+++ Level_0/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая на входе принимает число
+// и выдает его квадрат
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// - 2 147 483 648 до 2 147 483 647
+// double DoubleNumber = 4.454;
+// string StringStr = "Любая строка";
+
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+
+// bool b = number == square;
+
