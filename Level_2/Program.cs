8910a2f1b788231978834177ea689fc5b76e5f4d
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-27 00:53:27 +0300

    Написаны программы Lvl_2 и Lvl_4

--- Level_2/Program.cs
+++ Level_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 < number2)
+{
+    Console.Write($"max число: {number2}");
+}
+else if (number1 > number2)
+{
+    Console.Write($"max число: {number1}");
+}
+else if (number1 == number2)
+{
+    Console.Write($"числа {number1} и {number2} равны");
+}
--- Level_4/Program.cs
+++ Level_4/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int third = Convert.ToInt32(Console.ReadLine());
+
+if (first > second)
+{
+    if (first > third)
+    {
+         Console.WriteLine("{0} - наибольшее число", first);
+    }
+else
+    {
+        Console.WriteLine("{0} - наибольшее число", third);
+    }
+}
+else
+{
+    if (second > third)
+    {
+        Console.WriteLine("{0} - наибольшее число", second);
+    }
+else
+    {
+        Console.WriteLine("{0} - наибольшее число", third);
+    }
+}
