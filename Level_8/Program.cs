5924801b547d685f43cac4bcf372fe229e05d2f2
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-27 01:04:34 +0300

    Написаны программы Lvl_6 и Lvl_8

--- Level_8/Program.cs
+++ Level_8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (Num >= i)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+        i++;
+    }
+    i++;
+}
